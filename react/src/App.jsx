2b5063c886e3c3db7cf9332eed21a7abb5b4cd3c
bala00790 <ssbalaji2004@gmail.com>
2024-09-16 15:57:33 +0530

    updated

---
+++ react/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <div className='h-screen w-screen flex justify-center items-center bg-green-300 ' >
+        <div className='w-[30%] h-3/6 flex justify-center items-center bg-white rounded-md'>
+        <form className='w-[80%] h-[80%] flex flex-col justify-center items-center gap-4' >
+
+        <input type="text" name="" id="" placeholder="Name" className='p-3 rounded-sm bg-[#f8f8f8] w-full font-bold outline-none focus:border-b-2 hover:border-blue-300'/>
+        <input type="email" name="" id="" placeholder="Email" className='p-3 rounded-sm bg-[#f8f8f8] w-full font-bold outline-none focus:border-b-2 hover:border-blue-300' />
+        <input type="number" name="" id="" placeholder="Age" className='p-3 rounded-sm bg-[#f8f8f8] w-full font-bold outline-none focus:border-b-2 hover:border-blue-300'/>
+        <input type="password" name="" id="" placeholder="Password" className='p-3 rounded-sm bg-[#f8f8f8] w-full font-bold outline-none focus:border-b-2 hover:border-blue-300'/>
+        <button type="submit" className='w-full p-3  rounded-md outline-none font-bold text-white bg-green-300 '>Login</button> 
+
+        </form>   
+        </div>
+    </div>  
+
+    </>
+  )
+}
+
+export default App
