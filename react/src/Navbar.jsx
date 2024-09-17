b9f5af6dd938517a73ed772ac0540b609761d930
bala00790 <ssbalaji2004@gmail.com>
2024-09-17 14:19:40 +0530

    updated

--- react/src/Navbar.jsx
+++ react/src/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar =({data})=>{
     
     return(
-        <div className="w-full h-[3vh] bg-slate-500 flex justify-center items-center">
+        <div className="w-full h-[5vh] bg-slate-500 flex justify-center items-center">
          { data.map(datas => (
             <span>
                 {datas.appname}
