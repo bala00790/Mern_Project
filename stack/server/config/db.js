a51660b74f2b688501b8d03117dec4f11398dd3b
bala00790 <ssbalaji2004@gmail.com>
2024-09-24 16:10:53 +0530

    commit

--- stack/server/config/db.js
+++ stack/server/config/db.js
@@ -1,16 +1,28 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGOURL ||
-    "mongodb+srv://balaji2004:balaji2004@visitors.4lvly.mongodb.net/?retryWrites=true&w=majority&appName=visitors"
-);
+// mongoose.connect(
+//   process.env.MONGOURL ||
+//     "mongodb+srv://balaji2004:balaji2004@visitors.4lvly.mongodb.net/?retryWrites=true&w=majority&appName=visitors"
+// );
+
+// const connection = mongoose.connection;
+
+// connection.on("connected", () => {
+//   console.log("Db connected");
+// });
+// connection.on("erroe", () => {
+//   console.log("Db error");
+// });
+// module.exports = mongoose;
+
+
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGOURL)
 
 const connection = mongoose.connection;
+connection.on('connected', () => console.log("DB Connected"))
+connection.on('error', () => console.log("DB Error"))
 
-connection.on("connected", () => {
-  console.log("Db connected");
-});
-connection.on("erroe", () => {
-  console.log("Db error");
-});
-module.exports = mongoose;
+module.exports = mongoose
--- stack/server/server.js
+++ stack/server/server.js
@@ -1,15 +1,38 @@
-const express = require("express");
-const app = express();
-const dbconn = require("./config/db");
+// const express = require("express");
+// const app = express();
+// const dbconn = require("./config/db");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const port = process.env.PORT || 7777;
+// const port = process.env.PORT || 7777;
 
-app.get("/", (req, res) => {
-  res.json({ message: "welcome balajii" });
-});
+// app.get("/", (req, res) => {
+//   res.json({ message: "welcome balajii" });
+// });
+
+// app.listen(port, () => {
+//   console.log(`app is listening port....${port}`);
+// });
+
+
+
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+const dbConn = require('./config/db')
+const Projects = require('./routes/projectRoute')
+
+// const dotenv = require('dotenv')
+// dotenv.config()
+
+const port = process.env.PORT || 7778
+app.use('/projects', Projects)
+app.get('/', (req, res) => {
+    res.status(400).json("Welcome")
+})
 
 app.listen(port, () => {
-  console.log(`app is listening port....${port}`);
-});
+    console.log(`Server running in : ${port}`)
+})
